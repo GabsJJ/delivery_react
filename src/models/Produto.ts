cd0809c3a607c4463079dcadebdda06c1ce74004
Victor Hugo <projektvek@gmail.com>
2025-07-08 14:58:56 -0300

    :sparkles: Criado Models e Service

---
+++ src/models/Produto.ts
@@ -0,0 +1,21 @@
+import type Categoria from "./Categoria";
+import type Usuario from "./Usuario";
+
+export default interface Produto {
+  id: number;
+  nome: string;
+  descricao: string;
+  preco: number;
+  foto: string;
+  categoria: Categoria | undefined;
+  usuario: Usuario | undefined;
+  calorias: number;
+  energia: number;
+  acucares: number;
+  gordurasSaturadas: number;
+  sodio: number;
+  proteinas: number;
+  fibras: number;
+  frutas: number;
+  nutriscore: number;
+}
