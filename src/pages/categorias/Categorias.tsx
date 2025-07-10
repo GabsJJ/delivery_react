3787a25c3e328a487961c118603cb76e5a1fb5a3
Victor Hugo <projektvek@gmail.com>
2025-07-10 14:52:43 -0300

    :wrench: Alterado a posição do Botão

--- src/pages/categorias/Categorias.tsx
+++ src/pages/categorias/Categorias.tsx
@@ -4,7 +4,7 @@ import { Plus } from '@phosphor-icons/react'
 
 function Categorias() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto px-4 py-8">
       <div className="flex justify-end px-4">
         <Button className="bg-orange-600 hover:bg-orange-800 transition duration-300">
           <Plus size={32} color="#fafafa" />
