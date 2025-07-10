d4a8610c1e2632b2853bddb5405e3acbbb983fec
balbino-z <oviniciusbalbino@gmail.com>
2025-07-10 15:49:21 -0300

    :sparkles: Subindo o componente Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { Button } from './components/ui/button'
+import React from 'react';
+import HeroSection from './components/home/HeroSection';
+import './App.css';
 
 function App() {
-  
-
   return (
-    <Button>Clique aqui</Button>
-  )
+    <div className="App">
+      <HeroSection />
+    </div>
+  );
 }
 
-export default App
+export default App;
